4b89feb768d7405ed321aebe3622e6cbdd28f9a4
Lautaro <lautarojuarez0311@gmail.com>
2023-03-23 01:24:41 -0300

    first commit

---
+++ Main.java
@@ -0,0 +1,19 @@
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main (String[]args){
+     suma (10,30);
+
+     int a = 5;
+     System.out.println(a);
+    
+    }
+
+    public static void suma(int a, int b){
+      System.out.println(a+b);
+      System.out.println(a*b);
+      System.out.println(a/b);
+      System.out.println(a-b);
+    }
+}
